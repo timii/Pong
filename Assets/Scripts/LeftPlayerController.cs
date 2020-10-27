9e1c1e57b09c1a2c4022d4f09e9a3c9ca0427b87
Tim Thomas <inf3253@hs-worms.de>
2020-10-27 20:19:08 +0100

    added collision, deleted left and right wall

--- Assets/Scripts/LeftPlayerController.cs
+++ Assets/Scripts/LeftPlayerController.cs
@@ -5,33 +5,49 @@ using UnityEngine;
 public class LeftPlayerController : MonoBehaviour
 {
     private float movementSpeed = .02f;
+    private bool canMoveUp = true;
+    private bool canMoveDown = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Starting position = (-8.0, 0.0, -2.4)
-        Vector3 startingPosition = new Vector3 (-8.0f, 0.0f, -2.4f);
+        // Starting position = (-8.0, 0.0, 0.0)
+        Vector3 startingPosition = new Vector3 (-8.0f, 0.0f, 0.0f);
 
         // Set the player position to the starting position
         transform.position = startingPosition;
-
-        //Vector3 rightPlayerPosition = GameObject.Find("RightPlayer").transform.position;
-        //rightPlayerPosition = startingPosition;
-        Debug.Log(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
         // move up on "w" key hold
-        if (Input.GetKey(KeyCode.W)) {
+        if (canMoveUp == true && Input.GetKey(KeyCode.W)) {
             transform.Translate(0, movementSpeed, 0);
-            Debug.Log("W Key hold");
         }
         // move down on "s" key hold
-        if (Input.GetKey(KeyCode.S)) {
+        if (canMoveDown == true && Input.GetKey(KeyCode.S)) {
             transform.Translate(0, -movementSpeed, 0);
-            Debug.Log("S Key hold");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        Debug.Log("Hit: " + collider.name);
+        if (collider.name == "TopWall") {
+            canMoveUp = false;
+        }
+        else if (collider.name == "BottomWall") {
+            canMoveDown = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+        Debug.Log("Hit Exit: " + collider.name);
+        if (collider.name == "TopWall") {
+            canMoveUp = true;
+        }
+        else if (collider.name == "BottomWall") {
+            canMoveDown = true;
         }
     }
 }
