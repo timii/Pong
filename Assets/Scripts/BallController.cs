ff0c9437bebeab5b7f4517f18a151ce712b39d4b
Tim Thomas <inf3253@hs-worms.de>
2020-10-29 14:45:21 +0100

    Added 1s delay after ball respawning

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,27 +6,33 @@ public class BallController : MonoBehaviour
 {
     private float speedX;
     private float speedY;
+    private const float minSpeed = -0.02f;
+    private const float maxSpeed = 0.02f;
     public static int pointsRight;
     public static int pointsLeft;
+    private const float waitTime = 1.0f;
+    private float timer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Starting position = (0.0, 0.0, 0.0)
-        Vector3 startingPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+        CreateRandomMovement();
 
-        // Give the ball a random velocity and direction
-        speedX = Random.Range(-0.03f, 0.03f);
-        speedY = Random.Range(-0.04f, 0.04f);
+        ResetBall();
 
-        // Set the ball into the middle
-        transform.position = startingPosition;
+        //Wait();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speedX, speedY, 0);
+        timer += Time.deltaTime;
+
+        if (timer >= waitTime) {
+
+            // Move the ball in a random direction
+            transform.Translate(speedX, speedY, 0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
@@ -43,17 +49,61 @@ public class BallController : MonoBehaviour
         // If the ball scores a point on the right side, add a point to the right player
         // and reset the ball position
         else if (collider.name == "RightPointCounter") {
-            pointsRight++;
+            pointsLeft++;
+            ResetTimer();
             Start();
-            Debug.Log("Points: " + pointsLeft + ":" + pointsRight);
         }
 
         // If the ball scores a point on the left side, add a point to the left player
         // and reset the ball position
         else if (collider.name == "LeftPointCounter") {
-            pointsLeft++;
+            pointsRight++;
+            ResetTimer();
             Start();
-            Debug.Log("Points: " + pointsLeft + ":" + pointsRight);
         }
+    }
+
+    // Function to create random x and y speeds for the ball movement
+    private void CreateRandomMovement() {
+        float addSpeed = 0.01f; 
+
+        speedX = Random.Range(minSpeed, maxSpeed);
+        // Workaround to exclude values from -0.01 to 0.01
+        if (speedX > -addSpeed && speedX < 0) speedX -= addSpeed;
+        else if (speedX > 0 && speedX < addSpeed) speedX += addSpeed;
+
+        speedY = Random.Range(minSpeed, maxSpeed);
+        // Workaround to exclude values from -0.01 to 0.01
+        if (speedY > -addSpeed && speedY < 0) speedY -= addSpeed;
+        else if (speedY > 0 && speedY < addSpeed) speedY += addSpeed;
+    }
+
+    // Function to reset the ball position to the center
+    private void ResetBall() {
+        // Starting position = (0.0, 0.0, 0.0)
+        Vector3 startingPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+
+        // Set the ball into the middle
+        transform.position = startingPosition;
+    }
+
+    // Function to wait 1 second until the ball moves after being reset
+    private void Wait() {
+        System.DateTime currentTime = System.DateTime.Now;
+        Debug.Log("currentTime: " + currentTime);
+        System.DateTime goalTime = currentTime.AddSeconds(1);
+        Debug.Log("goalTime: " + goalTime);
+
+        // Stop the ball from moving 
+        while (currentTime <= goalTime) {
+            transform.Translate(0, 0, 0);
+            currentTime = System.DateTime.Now;
+            Debug.Log("currentTime in Loop" + currentTime);
+        }
+    }
+
+    private void ResetTimer() {
+        // Remove the recorded 2 seconds
+        timer = 0.0f;
     }
 }
