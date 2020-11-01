0038d01101321d28c2a333d81b188acbc7866eec
Tim Thomas <inf3253@hs-worms.de>
2020-11-01 01:42:42 +0100

    Added button functionality for start menu buttons

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+
+    // Function for the PlayButton to load the PlayScene
+    public void PlayButtonClick()
+    {
+        SceneManager.LoadSceneAsync("PlayScene");
+    }
+
+    // Function for the QuitButton to close the game
+    public void QuitButtonClick()
+    {
+        Debug.Log("Quit pressed");
+        Application.Quit();
+    }
+
+}
